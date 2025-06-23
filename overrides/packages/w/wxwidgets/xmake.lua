d8ed22a4195125976cbc2f4481517e2754c145be
Brickmasterhunt <brickmasterlogan@icloud.com>
2025-06-23 18:51:25 -0400

    modify some things for 3.3

--- overrides/packages/w/wxwidgets/xmake.lua
+++ overrides/packages/w/wxwidgets/xmake.lua
@@ -12,6 +12,8 @@ package("wxwidgets")
         add_versions("3.2.5", "0ad86a3ad3e2e519b6a705248fc9226e3a09bbf069c6c692a02acf7c2d1c6b51")
         add_versions("3.2.6", "939e5b77ddc5b6092d1d7d29491fe67010a2433cf9b9c0d841ee4d04acb9dce7")
         add_versions("3.2.7", "69a1722f874d91cd1c9e742b72df49e0fab02890782cf794791c3104cee868c6")
+        add_versions("3.2.8", "c74784904109d7229e6894c85cfa068f1106a4a07c144afd78af41f373ee0fe6")
+        add_versions("3.3.0", "492f5eb8a58715f2602f31025c3eaa20d71a3ec8e052c7a9d33153966b4badca")
 
         add_deps("cmake")
 
@@ -41,8 +43,11 @@ package("wxwidgets")
         )
     elseif is_plat("mingw") then
         add_defines("__WXMSW__")
-        add_syslinks("kernel32", "user32", "gdi32", "comdlg32", "winspool", "winmm", "shell32", "shlwapi", "comctl32", "ole32", "oleaut32",
-                     "uuid", "rpcrt4", "advapi32", "version", "ws2_32", "wininet", "oleacc", "uxtheme")
+        add_syslinks(
+            "kernel32", "user32", "gdi32", "winspool", "comdlg32", "advapi32", "shell32", "ole32",
+            "oleaut32", "uuid", "comctl32", "ws2_32", "opengl32", "shlwapi", "version", "oleacc",
+            "uxtheme", "imm32", "gdiplus", "msimg32"
+        )
     end
 
     on_load(function (package)
@@ -51,14 +56,7 @@ package("wxwidgets")
             local suffix = version:major() .. "." .. version:minor()
             local static = package:config("shared") and "" or "-static"
 
-            if package:is_plat("macosx") then
-                package:add("includedirs", path.join("lib", "wx", "config", "osx_cocoa-unicode" .. static .. "-" .. suffix))
-            elseif package:is_plat("linux") then
-                package:add("includedirs", path.join("lib", "wx", "config", "gtk3-unicode" .. static .. "-" .. suffix))
-            elseif package:is_plat("mingw") then
-                package:add("includedirs", path.join("lib", "wx", "config", "msw-unicode" .. static .. "-" .. suffix))
-            end
-
+            package:add("includedirs", path.join("include"))
             package:add("includedirs", path.join("include", "wx-" .. suffix))
             
             if package:debug() then
@@ -66,10 +64,14 @@ package("wxwidgets")
             end
             if package:config("shared") then
                 package:add("defines", "WXUSINGDLL")
-                package:add("deps", "libtiff", {configs = {shared = true}})
-                package:add("deps", "gdk-pixbuf", {configs = {shared = true}})
+                
+                package:add("deps", "libtiff", {system = false, configs = {shared = true}})
+
+                if is_plat("linux", "macosx") then
+                    package:add("deps", "gdk-pixbuf", {system = false, configs = {shared = true}})
+                end
             else
-                package:add("deps", "libtiff")
+                package:add("deps", "libtiff", {system = false})
 
                 if is_plat("linux", "macosx") then
                     package:add("deps", "gdk-pixbuf")
@@ -77,7 +79,7 @@ package("wxwidgets")
             end
 
             if is_plat("linux") then
-                 if package:config("shared") then
+                if package:config("shared") then
                     package:add("deps", "gtk3", {configs = {shared = true}})
                 else
                     package:add("deps", "gtk3")
@@ -96,23 +98,29 @@ package("wxwidgets")
             wprint("\ncmake may not find Cmath detail in https://github.com/prusa3d/PrusaSlicer/issues/12169\n")
         end
 
-        io.replace("build/cmake/modules/FindGTK3.cmake", "FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTK3 DEFAULT_MSG GTK3_INCLUDE_DIRS GTK3_LIBRARIES VERSION_OK)", 
-                                                         [[FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTK3 DEFAULT_MSG GTK3_INCLUDE_DIRS GTK3_LIBRARY_DIRS GTK3_LIBRARIES VERSION_OK)]], {plain = true})
-        local configs = {"-DwxBUILD_CXX_STANDARD=20",
-                         "-DwxUSE_UNICODE=ON",
-                         "-DwxBUILD_TESTS=OFF",
-                         "-DwxBUILD_SAMPLES=OFF",
-                         "-DwxBUILD_DEMOS=OFF",
-                         "-DwxBUILD_PRECOMP=ON",
-                         "-DwxBUILD_BENCHMARKS=OFF",
-                         "-DwxUSE_REGEX=sys",
-                         "-DwxUSE_ZLIB=sys",
-                         "-DwxUSE_EXPAT=sys",
-                         "-DwxUSE_LIBJPEG=sys",
-                         "-DwxUSE_LIBPNG=sys",
-                         "-DwxUSE_NANOSVG=sys",
-                         "-DwxUSE_LIBTIFF=builtin"
-                        }
+        io.replace(
+            "build/cmake/modules/FindGTK3.cmake",
+            "FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTK3 DEFAULT_MSG GTK3_INCLUDE_DIRS GTK3_LIBRARIES VERSION_OK)",
+            [[FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTK3 DEFAULT_MSG GTK3_INCLUDE_DIRS GTK3_LIBRARY_DIRS GTK3_LIBRARIES VERSION_OK)]],
+            {plain = true}
+        )
+
+        local configs = {
+            "-DwxBUILD_CXX_STANDARD=17",
+            "-DwxUSE_UNICODE=ON",
+            "-DwxBUILD_TESTS=OFF",
+            "-DwxBUILD_SAMPLES=OFF",
+            "-DwxBUILD_DEMOS=OFF",
+            "-DwxBUILD_PRECOMP=ON",
+            "-DwxBUILD_BENCHMARKS=OFF",
+            "-DwxUSE_REGEX=sys",
+            "-DwxUSE_ZLIB=sys",
+            "-DwxUSE_EXPAT=sys",
+            "-DwxUSE_LIBJPEG=sys",
+            "-DwxUSE_LIBPNG=sys",
+            "-DwxUSE_NANOSVG=sys",
+            "-DwxUSE_LIBTIFF=builtin"
+        }
         
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         if package:debug() then
@@ -134,14 +142,36 @@ package("wxwidgets")
         end
 
         if import("core.base.semver").satisfies(version:major() .. "." .. version:minor() .. "." .. version:patch(), "<3.2.7") then
-            os.cp(path.join(package:installdir("include", subdir, "wx", setupdir, "setup.h")),
-                  path.join(package:installdir("include", subdir, "wx")))
+            os.cp(
+                path.join(package:installdir("include", subdir, "wx", setupdir, "setup.h")),
+                path.join(package:installdir("include", subdir, "wx"))
+            )
         else
-            os.cp(path.join(package:cachedir(), "source", "include", "wx", setupdir, "setup.h"),
-                  path.join(package:installdir("include", subdir, "wx")))
+            os.cp(
+                path.join(package:cachedir(), "source", "include", "wx", setupdir, "setup.h"),
+                path.join(package:installdir("include", subdir, "wx"))
+            )
         end
-        
-        local lib_suffix = version:major() .. "." .. version:minor()
+
+        local lib_suffix = version:major() .. version:minor()
+
+        local libsrc = (os.dirs(path.join(package:installdir("lib"), "*_lib"))[1])
+        if libsrc then
+            for _, file in ipairs(os.files(path.join(libsrc, "*"))) do
+                os.mv(file, package:installdir("lib"))
+            end
+            os.rmdir(libsrc)
+        end
+
+        if package:config("shared") and package:is_plat("mingw") then
+            for _, file in ipairs(os.files(path.join(package:installdir("lib"), "*-" .. lib_suffix .. ".dll.a"))) do
+                local newname = file:gsub("%.dll%.a$", ".a")
+                os.mv(file, newname)
+            end
+        end
+
+        os.rmdir(path.join(package:installdir("lib"), "cmake"))
+
         if package:is_plat("linux") then
             package:add("links", "wx_gtk3u_xrc-" .. lib_suffix, "wx_gtk3u_html-" .. lib_suffix, "wx_gtk3u_qa-" .. lib_suffix, "wx_gtk3u_core-" .. lib_suffix, "wx_baseu_xml-" .. lib_suffix, "wx_baseu_net-" .. lib_suffix, "wx_baseu-" .. lib_suffix)
         end
--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,5 @@
 set_project("wxwidgets-testing")
-set_languages("c++20")
+set_languages("c++17")
 
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
@@ -29,6 +29,7 @@ target("test")
     add_packages("wxwidgets")
     add_cxxflags("-static", {force = true})
     -- add_cxxflags("-fno-inline-functions", {force = true})
+    add_cxxflags("-D_LIBCPP_HAS_NO_VENDOR_AVAILABILITY_ANNOTATIONS", "-D_LIBCPP_HARDENING_MODE_DEFAULT=_LIBCPP_HARDENING_MODE_NONE")
     if is_os("windows") then
         add_files("resources/windows/**.rc")
     end
